5c7f322a0436c40750b39975901ab86e1d351aec
Daniel <dperezterron19@gmail.com>
2026-06-11 01:19:36 +0200

    Decals + Arreglar lvl1

---
+++ Assets/Assets/Scripts/SHADERS/AnimateDecals.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class AnimateDecals : MonoBehaviour
+{
+    public Material ullsOberts;
+    public Material ullsTancats;
+
+    private DecalProjector decalProjector;
+
+    private void Start()
+    {
+        decalProjector = GetComponent<DecalProjector>();
+        StartCoroutine(parpadeoDecals());
+    }
+
+    IEnumerator parpadeoDecals()
+    {
+        while (true)
+        {
+            decalProjector.material = ullsTancats;
+            yield return new WaitForSeconds(0.07f);
+            decalProjector.material = ullsOberts;
+            yield return new WaitForSeconds(3f);
+            decalProjector.material = ullsTancats;
+            yield return new WaitForSeconds(0.07f);
+            decalProjector.material = ullsOberts;
+            yield return new WaitForSeconds(3f);
+            decalProjector.material = ullsTancats;
+            yield return new WaitForSeconds(0.1f);
+            decalProjector.material = ullsOberts;
+            yield return new WaitForSeconds(0.3f);
+            decalProjector.material = ullsTancats;
+            yield return new WaitForSeconds(0.05f);
+            decalProjector.material = ullsOberts;
+            yield return new WaitForSeconds(5f);
+        }
+        
+    }
+}
